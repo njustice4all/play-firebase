07e3b4a988af38f67374f19bfee4f33981fb239e
youngjoo <hyj@aty.kr>
2018-03-07 21:21:49 +0900

    Create collection/document/data

--- src/index.js
+++ src/index.js
@@ -3,8 +3,41 @@ import serviceAccount from '../config/serviceAccountKey.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://play-firebase-a6706.firebaseio.com',
 });
 
 const db = admin.firestore();
 
-console.log(db);
+/**
+ * phone 컬렉션 만들어서 DOC samsung { brand: ... 입력 }
+ */
+const phoneRef = db.collection('phone').doc('samsung');
+
+const setPhone = phoneRef.set({
+  brand: 'samsung',
+  device: 'galaxy note 3',
+});
+
+/**
+ * phone 모델의 apple 추가
+ */
+const appleRef = db.collection('phone').doc('apple');
+
+const setApple = appleRef.set({
+  brand: 'apple',
+  device: 'iPhone X',
+  color: 'black',
+});
+
+// get data
+db
+  .collection('phone')
+  .get()
+  .then(snapshot => {
+    snapshot.forEach(doc => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });
